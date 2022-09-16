0c13e119bd8cefe977bb912169179f109b5f4f0b
NithyaJain <nithyajain32@gmail.com>
2022-09-16 20:29:15 +0530

    Commit

--- src/app/link/link.component.ts
+++ src/app/link/link.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-link',
   templateUrl: './link.component.html',
@@ -13,6 +14,7 @@ export class LinkComponent implements OnInit {
   Products: any[] = [
     {
       "productname": "Bag"
+      
       
     },
     {
@@ -68,45 +70,46 @@ export class LinkComponent implements OnInit {
       "pid": "100",
       "pname": "Sling",
       "status": "Active",
-      "color": "green"
-      
+      "color": "green",
+      "img":"https://images.unsplash.com/photo-1587467512961-120760940315?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNsaW5nJTIwYmFnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+     
       
     },
     {
       "pid": "101",
       "pname": "Shoes",
       "status": "InActive",
-      "color": "red"
+      "color": "red",
+      "img":"https://images.unsplash.com/photo-1562183241-b937e95585b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNob2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60 "
     },
     {
       "pid": "102",
       "pname": "Watch",
       "status": "Out of Stock",
-      "color": "yellow"
+      "color": "yellow",
+      "img":"https://images.unsplash.com/photo-1511370235399-1802cae1d32f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhdGNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
       "pid": "103",
       "pname": "Wrist Watch",
       "status": "Active",
-      "color": "green"
+      "color": "green",
+      "img":"https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2h8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60" 
     },
     {
       "pid": "104",
       "pname": "Trolly",
       "status": "InActive",
-      "color": "red"
+      "color": "red",
+      "img":"https://media.istockphoto.com/photos/packing-suitcase-for-travel-vacation-in-new-normal-top-view-picture-id1311253634?b=1&k=20&m=1311253634&s=170667a&w=0&h=jQKtsG95TDft5UXq9tZldkusWH-Ge6DAOqoyYGZZres="
     },
-    {
-      "pid": "105",
-      "pname": "Formal Shoes",
-      "status": "Active",
-      "color": "green"
-    },
+   
     {
       "pid": "106",
       "pname": "Wallet",
       "status": "Out of Stock",
-      "color": "yellow"
+      "color": "yellow",
+      "img":"https://images.unsplash.com/photo-1614330316655-51dbca10f5f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbGV0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
     }
   ]
   getColor() {
